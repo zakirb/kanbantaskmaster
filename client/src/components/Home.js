41d43ba370375b18e8d74a95cff91b90445cb218
danielvancura <danielvancura@gmail.com>
2018-03-23 10:05:50 -0700

    10:05am

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-const style = {
-  zDepth: 15,
-  margin: 20,
-}
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { Row, Col } from 'react-flexbox-grid';
+import ImageCarousel from './Carousel.js';
 
+const style = {
+card_style: {
+  width: 1250,
+  height: 300,
+  margin: 5,
+  textAlign: 'center'
+}
+}
 class Home extends Component {
   render(){
     return(
+      <div>
+      <Row center="xs">
+        <Col>
       <div className='row'>
-        <Paper style={style}>
+
+        <Card  className="home" style={style.card_style}  zDepth={5}>
           <div className='col s12 z-depth-3'>
           <h1 className="home">Task Master</h1>
           <h2 className="home">Welcome to Task Master! Our site was developed to help organize projects more efficiently
           based on Scrum principles. Whether you’re in a business setting or a working parent, our site helps you monitor
           the progress of each project. Simply sign up and you can begin creating projects and assigning tasks. </h2>
         </div>
-        </Paper>
+        </Card>
+
       </div>
+      </Col>
+      </Row>
+      <ImageCarousel />
+      </div>
+
+
     )
   }
 }
