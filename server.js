be16b1d34f8a51339aa6eb7f99e74c9a0a7b6b5d
Zakir Butte <zakir.butte@gmailcom>
2018-03-26 09:25:52 -0700

    heroku deployment round 2

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser');
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/projectworkfloworganizer');
+// mongoose.connect('mongodb://localhost/projectworkfloworganizer');
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 
 var auth = require('./routes/auth');
@@ -20,7 +21,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 // Do we still need this?
 app.use(function(req, res, next) {
@@ -34,6 +36,11 @@ app.use('/create', create);
 app.use('/edit', edit);
 app.use('/view', view);
 app.use('/destroy', destroy)
+
+app.get('*', function(req, res, next) {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 
