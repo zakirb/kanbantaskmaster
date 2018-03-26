d14f1d4f5f7c26c942062ffb88510d3c0c457cbd
Zakir Butte <zakir.butte@gmailcom>
2018-03-26 09:44:03 -0700

    heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 // Mongoose stuff
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/projectworkfloworganizer');
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI);
 
 
 var auth = require('./routes/auth');
